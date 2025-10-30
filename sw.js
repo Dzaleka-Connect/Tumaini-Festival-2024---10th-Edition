2bbd8feb5f11d917e7d073337a760aa05a56151f
Bakari Mustafa <bakarimussa06@gmail.com>
2025-10-30 11:51:35 +1000

    Tumaini Festival 2025 Program

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'tumaini-cache-v1';
+const CORE_ASSETS = [
+  '/',
+  '/index.html',
+  '/assets/css/style.css',
+  '/assets/js/main.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(CORE_ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => Promise.all(keys.map((k) => k !== CACHE_NAME && caches.delete(k))))
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.method !== 'GET') return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      const network = fetch(event.request).then((resp) => {
+        const copy = resp.clone();
+        caches.open(CACHE_NAME).then((cache) => cache.put(event.request, copy));
+        return resp;
+      }).catch(() => cached);
+      return cached || network;
+    })
+  );
+});
+
+
